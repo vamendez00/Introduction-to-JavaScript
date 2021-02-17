e5b929b83f99c4f10421fc52303d4ab1ca741525
vamendez00 <veromendez@gmail.com>
2021-02-16 20:00:50 -0600

    All tasks completed. All tasks passed to satisfy codeGrade. Task 4, however, still not properly executed.

--- index.js
+++ index.js
@@ -170,10 +170,22 @@ Use the game function below to do the following:
 */
 
 function game(user, computer){
-    /*add your code here*/
-}
-  
-  
+  if (user === computer) {
+    return ("it's a tie");
+  } else if ((user === "rock") && (computer === "paper")) {
+    return ("you lose!");
+    } else if ((user === "rock") && (computer === "scissors")) {
+      return ("you win!");
+      } else if ((user === "scissors") && (computer === "rock")) {
+        return ("you lose!");
+        } else if ((user === "scissors") && (computer === "paper")) {
+          return ("you win!");
+          } else if ((user === "paper") && (computer === "scissors")) {
+            return ("you lose!");
+            } else if ((user === "paper") && (computer === "rock")) {
+              return ("you win!");
+              } 
+}  
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
